1edc8491efbffe2db1dd5a9e2f63369495002d6c
ftp <andree3d_git@salvage.beget.ru>
2026-05-07 13:36:07 +0300

    Вывод краткого описания дизайнера в списке

--- local/templates/main/components/bitrix/news/dizaynery/bitrix/news.list/.default/template.php
+++ local/templates/main/components/bitrix/news/dizaynery/bitrix/news.list/.default/template.php
@@ -15,14 +15,18 @@ $this->setFrameMode(true);
 
 
 <?foreach($arResult["ITEMS"] as $arItem):?>
-	<?
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-	?>
-<div class="col-lg-4 col-md-4 col-sm-4">
-    <a class="dizayner-link" id="<?=$this->GetEditAreaId($arItem['ID']);?>" data-id="1583" href="<?=$arItem['DETAIL_PAGE_URL']?>" title="<?=$arItem['NAME']?>"><img src="<?=$arItem['DETAIL_PICTURE']['SRC']?>" alt="<?=$arItem['NAME']?>">
-        <div class="name_designer"><?=$arItem['NAME']?></div>
-    </a>
-</div>
+    <?
+    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+    ?>
+    <div class="col-lg-4 col-md-4 col-sm-4">
+        <a class="dizayner-link" id="<?=$this->GetEditAreaId($arItem['ID']);?>" data-id="1583" href="<?=$arItem['DETAIL_PAGE_URL']?>" title="<?=$arItem['NAME']?>"><img src="<?=$arItem['DETAIL_PICTURE']['SRC']?>" alt="<?=$arItem['NAME']?>">
+            <div class="name_designer"><?=$arItem['NAME']?></div>
+            <?if($arItem['PREVIEW_TEXT']):?>
+                <div class="after-name"><?=$arItem['PREVIEW_TEXT']?></div>
+            <?endif;?>
+        </a>
+    </div>
 <?endforeach;?>
+
 
